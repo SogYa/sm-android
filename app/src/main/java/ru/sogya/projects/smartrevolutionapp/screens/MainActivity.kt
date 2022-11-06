9fecbd60452319ef145be15f82837c8a87ffa693
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2022-11-06 13:42:09 +0300

    added splash screen

--- app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/MainActivity.kt
+++ app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/MainActivity.kt
@@ -3,14 +3,15 @@ package ru.sogya.projects.smartrevolutionapp.screens
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import ru.sogya.projects.smartrevolutionapp.R
 import ru.sogya.projects.smartrevolutionapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_SplashScreen)
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
--- app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/start/StartFragment.kt
+++
@@ -1,7 +0,0 @@
-package ru.sogya.projects.smartrevolutionapp.screens.start
-
-import androidx.fragment.app.Fragment
-import ru.sogya.projects.smartrevolutionapp.R
-
-class StartFragment : Fragment(R.layout.fragment_start){
-}
