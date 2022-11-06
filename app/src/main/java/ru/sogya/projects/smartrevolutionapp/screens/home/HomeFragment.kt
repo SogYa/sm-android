0a18368229842380dfae7520f4267fe79a5c8e99
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2022-11-06 17:10:40 +0300

    changed design

--- app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/home/HomeFragment.kt
+++ app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/home/HomeFragment.kt
@@ -1,4 +1,7 @@
 package ru.sogya.projects.smartrevolutionapp.screens.home
 
-class HomeFragment {
+import androidx.fragment.app.Fragment
+import ru.sogya.projects.smartrevolutionapp.R
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
 }
