fa316a5b303269296eb914162d155dcd73712010
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2022-11-06 14:38:29 +0300

    added android navigation

--- app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/MainActivity.kt
+++ app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/MainActivity.kt
@@ -3,11 +3,14 @@ package ru.sogya.projects.smartrevolutionapp.screens
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import ru.sogya.projects.smartrevolutionapp.R
 import ru.sogya.projects.smartrevolutionapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_SplashScreen)
@@ -15,7 +18,14 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        setupNavigation()
     }
 
+
+    private fun setupNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+
+    }
 }
