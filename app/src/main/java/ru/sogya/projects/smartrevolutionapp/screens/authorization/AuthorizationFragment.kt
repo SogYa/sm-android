f08e8ad3ad307be24129cd3792227b5150502027
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2022-11-09 22:28:34 +0300

    added navigate to home fragment

--- app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/authorization/AuthorizationFragment.kt
+++ app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/authorization/AuthorizationFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import retrofit2.Call
@@ -40,6 +41,8 @@ class AuthorizationFragment : Fragment(R.layout.fragment_authorization) {
                 uri = uri.substring(0, uri.length - 1)
                 binding.editTextUri.setText(uri)
             }
+            // https://96890a116fb0.sn.mynetname.net/
+            // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI2NWI0YmU1YjllMzA0YzI3ODEwMDdhNzUyNTM2ZjZjMCIsImlhdCI6MTY2NzMzNDEzNiwiZXhwIjoxOTgyNjk0MTM2fQ.tPyqb82IKVuAUJlH4k0LY_U54ta9GNjGHGPZ5MaSHKs
             NetworkService.getretrofitService(uri).getApiMessage("Bearer $password")
                 .enqueue(object : Callback<Message> {
                     override fun onResponse(
@@ -49,12 +52,12 @@ class AuthorizationFragment : Fragment(R.layout.fragment_authorization) {
                         when (response.code()) {
                             200 -> {
                                 Toast.makeText(context, "Access granted", Toast.LENGTH_SHORT).show()
-                                findNavController().navigate(R.id.action_authorizationFragment_to_homeFragment)
+                                findNavController().navigate(R.id.action_authorizationFragment_to_homeFragment,
+                                        bundleOf("response" to response.body()?.message.toString()))
                             }
                             201 -> Toast.makeText(context, "Invalid data", Toast.LENGTH_SHORT)
                                 .show()
                         }
-
                     }
 
                     override fun onFailure(call: Call<Message>, t: Throwable) {
@@ -62,23 +65,6 @@ class AuthorizationFragment : Fragment(R.layout.fragment_authorization) {
                     }
 
                 })
-
-            //Запрос всех сущностей лаборатории
-
-//            NetworkService.getretrofitService(uri).getApiStates("Bearer $password")
-//                .enqueue(object : Callback<List<State>> {
-//                    override fun onResponse(
-//                        call: Call<List<State>>,
-//                        response: Response<List<State>>
-//                    ) {
-//                        Log.d("STATES", response.body().toString())
-//                    }
-//
-//                    override fun onFailure(call: Call<List<State>>, t: Throwable) {
-//                        TODO("Not yet implemented")
-//                    }
-//
-//                })
         }
     }
 }
