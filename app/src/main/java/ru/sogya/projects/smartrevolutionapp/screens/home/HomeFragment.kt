5a71284dce7362836f3fd3ff242ef5f6883f783f
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2022-11-16 12:06:18 +0300

    changed realization and added viewmodel

--- app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/home/HomeFragment.kt
+++ app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/home/HomeFragment.kt
@@ -5,12 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import ru.sogya.projects.smartrevolutionapp.R
 import ru.sogya.projects.smartrevolutionapp.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
     private lateinit var binding: FragmentHomeBinding
+    private val vm: HomeVM by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,15 +25,19 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //Получение всех стейтов лаборатории
-
-        binding.textView.text = arguments?.getString("response")
-
         binding.button.setOnClickListener {
+            vm.logOut()
             findNavController().popBackStack()
         }
 
     }
 
+    override fun onStart() {
+        super.onStart()
+        vm.getMessage()
+        vm.messageLiveData.observe(viewLifecycleOwner) {
+            binding.textView.text = it
+        }
+    }
 
 }
---
+++ app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/home/HomeVM.kt
@@ -0,0 +1,41 @@
+package ru.sogya.projects.smartrevolutionapp.screens.home
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.sogya.data.repository.NetworkRepositoryImpl
+import com.sogya.data.utils.Constants
+import com.sogya.domain.models.Message
+import com.sogya.domain.usecases.GetMessageUseCase
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
+import ru.sogya.projects.smartrevolutionapp.needtoremove.SPControl
+
+class HomeVM : ViewModel() {
+    val messageLiveData = MutableLiveData<String>()
+    private val repository = NetworkRepositoryImpl()
+    private val getMessageUseCase = GetMessageUseCase(repository)
+
+    fun getMessage() {
+        getMessageUseCase.invoke(
+            SPControl.getIstance().getStringPrefs(Constants.URI),
+            "Bearer ${SPControl.getIstance().getStringPrefs(Constants.AUTH_TOKEN)}"
+        ).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+            .subscribe(object : DisposableSingleObserver<Message>() {
+                override fun onSuccess(t: Message) {
+                    messageLiveData.value = t.message
+
+                }
+
+                override fun onError(e: Throwable) {
+                    Log.d("HOME_ERROR", e.message.toString())
+                }
+
+            })
+    }
+
+    fun logOut() {
+        SPControl.getIstance().updatePrefs(Constants.AUTH_TOKEN, " ")
+    }
+}
