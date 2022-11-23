5bceff2cdd770546522a91857adc029ec6564345
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2022-11-23 12:58:34 +0300

    deleted useless methods

--- app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/authariztion/AuthorizationVM.kt
+++ app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/authariztion/AuthorizationVM.kt
@@ -5,9 +5,7 @@ import androidx.lifecycle.ViewModel
 import com.sogya.data.repository.NetworkRepositoryImpl
 import com.sogya.data.utils.Constants
 import com.sogya.data.utils.myCallBack
-import com.sogya.domain.models.Message
 import com.sogya.domain.models.TokenInfo
-import com.sogya.domain.usecases.GetMessageUseCase
 import com.sogya.domain.usecases.GetTokenUseCase
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.observers.DisposableSingleObserver
@@ -17,24 +15,7 @@ import ru.sogya.projects.smartrevolutionapp.needtoremove.SPControl
 
 class AuthorizationVM : ViewModel() {
     private val repository = NetworkRepositoryImpl()
-    private val getMessageUseCase = GetMessageUseCase(repository)
     private val getTokenUseCase = GetTokenUseCase(repository)
-
-    fun getMessage(baseUri: String, token: String) {
-        getMessageUseCase.invoke(baseUri, "Bearer $token")
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(object : DisposableSingleObserver<Message>() {
-                override fun onSuccess(t: Message) {
-                    Log.d("TestSuc", t.message)
-                }
-
-                override fun onError(e: Throwable) {
-                    Log.d("TestSuc", e.message.toString())
-                }
-
-            })
-    }
 
 
     fun getToken(baseUri: String, authCode: String, myCallBack: myCallBack<Boolean>) {
@@ -49,7 +30,7 @@ class AuthorizationVM : ViewModel() {
                 }
 
                 override fun onError(e: Throwable) {
-                    Log.d("TOKENERROR", e.message.toString())
+                    Log.d("TOKEN_ERROR", e.message.toString())
                     myCallBack.error()
                 }
             })
