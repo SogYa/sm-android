fb49827592557241ad5a170e63f1f4519964dcff
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2022-11-23 12:56:33 +0300

    migration to single activity pattern

--- app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/authariztion/AuthFragment.kt
+++ app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/authariztion/AuthFragment.kt
@@ -2,26 +2,40 @@ package ru.sogya.projects.smartrevolutionapp.screens.authariztion
 
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
+import android.view.ViewGroup
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Toast
-import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.sogya.data.utils.Constants
 import com.sogya.data.utils.myCallBack
-import ru.sogya.projects.smartrevolutionapp.databinding.ActivityWebViewBinding
+import ru.sogya.projects.smartrevolutionapp.R
+import ru.sogya.projects.smartrevolutionapp.databinding.FragmentWebViewBinding
 
-class AuthActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityWebViewBinding
+class AuthFragment : Fragment(R.layout.fragment_web_view) {
+    private lateinit var binding: FragmentWebViewBinding
     lateinit var uri: String
     private val vm: AuthorizationVM by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityWebViewBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentWebViewBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
 
         binding.buttonConnect.setOnClickListener {
             uri = binding.editTextUri.text.toString()
@@ -30,6 +44,7 @@ class AuthActivity : AppCompatActivity() {
                 binding.editTextUri.setText(uri)
 
             }
+            val redirectUri = "${uri.replace(" https ://", Constants.REDIRECT_URI)}/auth_callback"
 
             binding.logInWebView.visibility = VISIBLE
             binding.loginConstraint.visibility = GONE
@@ -38,7 +53,7 @@ class AuthActivity : AppCompatActivity() {
             binding.logInWebView.loadUrl(
                 "$uri/auth/authorize?" +
                         "client_id=$uri" +
-                        "&redirect_uri=${uri}/auth_callback"
+                        "&redirect_uri=${redirectUri}"
             )
             binding.logInWebView.webViewClient = object : WebViewClient() {
                 override fun shouldOverrideUrlLoading(
@@ -51,14 +66,18 @@ class AuthActivity : AppCompatActivity() {
                                 binding.loadingConstraint.visibility = VISIBLE
                                 vm.getToken(uri, it, object : myCallBack<Boolean> {
                                     override fun data(t: Boolean) {
-                                        Log.d("AUTHCODE", it)
-                                        finish()
+                                        Toast.makeText(
+                                            context,
+                                            getString(R.string.auth_succes),
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                        findNavController().navigate(R.id.action_authFragment_to_homeFragment)
                                     }
 
                                     override fun error() {
                                         Toast.makeText(
-                                            this@AuthActivity,
-                                            "Authorization failed",
+                                            context,
+                                            getString(R.string.auth_failed),
                                             Toast.LENGTH_SHORT
                                         ).show()
                                     }
