7792c8e6efdead30248b0e338390a1683bd3ab09
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2022-11-23 12:59:55 +0300

    added interceptor

---
+++ data/src/main/java/com/sogya/data/api/interceptors/AuthInterceptor.kt
@@ -0,0 +1,13 @@
+package com.sogya.data.api.interceptors
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class AuthInterceptor(private val token: String) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request().newBuilder()
+            .addHeader("Authorization", "Bearer $token")
+            .build()
+        return chain.proceed(request)
+    }
+}
