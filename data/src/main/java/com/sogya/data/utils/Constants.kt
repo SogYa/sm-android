2eecd567ae78ea868f696014ca61576ac0c16d7c
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2022-11-23 12:55:48 +0300

    added new constan

--- data/src/main/java/com/sogya/data/utils/Constants.kt
+++ data/src/main/java/com/sogya/data/utils/Constants.kt
@@ -2,6 +2,6 @@ package com.sogya.data.utils
 
 object Constants {
     const val AUTH_TOKEN = "auth_token"
-    // const val REDIRECT_URI = "ru.sogya.projects.smartrevolutionapp://"
+    const val REDIRECT_URI = "ru.sogya.projects.smartrevolutionapp://"
     const val URI = "uri"
 }
