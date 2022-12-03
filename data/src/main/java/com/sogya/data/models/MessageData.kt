9a719314c1b174a1e5334bc54c6a37e59a5e7113
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2022-12-03 21:36:17 +0300

    renamed

--- data/src/main/java/com/sogya/data/models/MessageData.kt
+++ data/src/main/java/com/sogya/data/models/MessageData.kt
@@ -2,7 +2,7 @@ package com.sogya.data.models
 
 import com.google.gson.annotations.SerializedName
 
-data class Message(
+data class MessageData(
     @SerializedName("message")
     val message:String
 )
