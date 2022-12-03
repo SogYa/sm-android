7e86f89f3a89ea50eadb00b4946d3a3ebe86b040
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2022-12-03 14:23:55 +0300

    added model to auth message

---
+++ data/src/main/java/com/sogya/data/models/requests/AuthMessage.kt
@@ -0,0 +1,12 @@
+package com.sogya.data.models.requests
+
+import com.google.gson.annotations.SerializedName
+
+
+data class AuthMessage(
+    @SerializedName("type")
+    val type: String = "auth",
+    @SerializedName("access_token")
+    val token: String
+
+)
