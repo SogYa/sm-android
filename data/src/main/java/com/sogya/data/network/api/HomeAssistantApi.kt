fd253fe702af8975f3239488f59f0ad41c11224c
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2022-12-03 21:38:27 +0300

    chore

--- data/src/main/java/com/sogya/data/network/api/HomeAssistantApi.kt
+++ data/src/main/java/com/sogya/data/network/api/HomeAssistantApi.kt
@@ -1,24 +1,23 @@
 package com.sogya.data.network.api
 
 
-import com.sogya.domain.models.Message
+import com.sogya.data.models.MessageData
 import com.sogya.data.models.State
 import com.sogya.domain.models.TokenInfo
 import io.reactivex.Single
-import okhttp3.HttpUrl
 import retrofit2.http.*
 
 interface HomeAssistantApi {
 
     @GET("/api/")
-    fun getApiMessage(@Header("Authorization") token: String): Single<Message>
+    fun getApiMessage(@Header("Authorization") token: String): Single<MessageData>
 
     @GET("/api/states")
     fun getApiStates(@Header("Authorization") token: String): Single<List<State>>
 
     @FormUrlEncoded
     @POST("/auth/token")
-     fun getApiToken(
+    fun getApiToken(
         @Field("grant_type") grandType: String,
         @Field("code") code: String,
         @Field("client_id") clientId: String
