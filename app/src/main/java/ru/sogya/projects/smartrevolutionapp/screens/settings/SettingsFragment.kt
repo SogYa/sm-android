87e3c42e7759d9b7a952567ac2acfc9dc5b5be88
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2022-12-03 14:22:00 +0300

    added settings fragment

---
+++ app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/settings/SettingsFragment.kt
@@ -0,0 +1,8 @@
+package ru.sogya.projects.smartrevolutionapp.screens.settings
+
+import androidx.fragment.app.Fragment
+import ru.sogya.projects.smartrevolutionapp.R
+
+
+class SettingsFragment : Fragment(R.layout.fragment_settings) {
+}
