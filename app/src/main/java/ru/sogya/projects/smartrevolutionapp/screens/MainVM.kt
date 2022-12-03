bc71b34388970b70b549843a11223ad98b48cb65
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2022-12-03 14:22:47 +0300

    added exit from test mode

--- app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/MainVM.kt
+++ app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/MainVM.kt
@@ -7,6 +7,8 @@ import ru.sogya.projects.smartrevolutionapp.needtoremove.SPControl
 class MainVM : ViewModel() {
 
     fun logOut() {
-        SPControl.getIstance().updatePrefs(Constants.AUTH_TOKEN, "")
+        SPControl.getInstance().updatePrefs(Constants.AUTH_TOKEN, "")
+        SPControl.getInstance().updatePrefs(Constants.URI, "")
+        SPControl.getInstance().updatePrefs(Constants.TEST_MODE, false)
     }
 }
