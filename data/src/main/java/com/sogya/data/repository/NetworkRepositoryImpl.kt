df268e5aa95dd04000f6ed13af83d447f4a2a7fb
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2022-12-03 21:37:44 +0300

    added mapper for message class

--- data/src/main/java/com/sogya/data/repository/NetworkRepositoryImpl.kt
+++ data/src/main/java/com/sogya/data/repository/NetworkRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.sogya.data.repository
 
+import com.sogya.data.mappers.MessageMapper
 import com.sogya.data.network.api.NetworkService
 import com.sogya.domain.models.Message
 import com.sogya.domain.models.TokenInfo
@@ -8,7 +9,9 @@ import io.reactivex.Single
 
 class NetworkRepositoryImpl : NetworkRepository {
     override fun getMessage(baseUri: String, token: String): Single<Message> {
-        return NetworkService.getRetrofitService(baseUri).getApiMessage(token)
+        return NetworkService.getRetrofitService(baseUri).getApiMessage(token).map {
+            return@map MessageMapper(it).toMessageDomian()
+        }
     }
 
     override fun getToken(baseUri: String, authCode: String): Single<TokenInfo> {
