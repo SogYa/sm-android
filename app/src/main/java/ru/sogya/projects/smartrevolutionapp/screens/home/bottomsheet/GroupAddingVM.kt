31c628a8aabaeef18d8e0a0d04a8403048d3527d
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2022-12-18 17:49:04 +0300

    added group fragment to bottom sheet

---
+++ app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/home/bottomsheet/GroupAddingVM.kt
@@ -0,0 +1,10 @@
+package ru.sogya.projects.smartrevolutionapp.screens.home.bottomsheet
+
+import androidx.lifecycle.ViewModel
+
+class GroupAddingVM : ViewModel() {
+
+    fun createNewGroup(groupTag : String, groupDesc: String?){
+
+    }
+}
