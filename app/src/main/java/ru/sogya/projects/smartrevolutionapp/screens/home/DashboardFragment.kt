092ffeab01ee57232fe1521d9d878c8501ab9634
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2022-12-25 14:09:05 +0300

    added get all items from room method

--- app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/home/DashboardFragment.kt
+++ app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/home/DashboardFragment.kt
@@ -1,19 +1,25 @@
 package ru.sogya.projects.smartrevolutionapp.screens.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.sogya.domain.models.StateDomain
 import ru.sogya.projects.smartrevolutionapp.R
 import ru.sogya.projects.smartrevolutionapp.databinding.FragmentDashboardBinding
 import ru.sogya.projects.smartrevolutionapp.dialogs.LogOutDialogFragment
 import ru.sogya.projects.smartrevolutionapp.screens.home.bottomsheet.DashboardBottomSheet
+import ru.sogya.projects.smartrevolutionapp.screens.home.bottomsheet.stateadding.StateAdapter
 
-class DashboardFragment : Fragment(R.layout.fragment_dashboard) {
+class DashboardFragment : Fragment(R.layout.fragment_dashboard), StateAdapter.OnStateClickListener {
     private lateinit var binding: FragmentDashboardBinding
     private val vm: DashboardVM by viewModels()
+    private lateinit var adapter: StateAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -27,13 +33,26 @@ class DashboardFragment : Fragment(R.layout.fragment_dashboard) {
         binding.addButton.setOnClickListener {
             DashboardBottomSheet().show(childFragmentManager, LogOutDialogFragment().tag)
         }
+        binding.statesRecyclerView.layoutManager = LinearLayoutManager(context)
+        adapter = StateAdapter(this)
+        binding.statesRecyclerView.adapter = adapter
+
     }
 
-    override fun onStart() {
-        super.onStart()
+    override fun onResume() {
+        super.onResume()
+        vm.getItemsFromLocalDB()
         vm.loadingViewLiveData.observe(viewLifecycleOwner) {
             binding.loadingView.visibility = it
         }
+        vm.itemsLiveData.observe(viewLifecycleOwner) {
+            adapter.updateStatesList(it)
+            Log.d("ListStates", it.toString())
+        }
+    }
+
+    override fun onClick(stateDomain: StateDomain) {
+        Toast.makeText(context, "Coming soon..", Toast.LENGTH_SHORT).show()
     }
 
 }
--- app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/home/DashboardVM.kt
+++ app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/home/DashboardVM.kt
@@ -1,14 +1,35 @@
 package ru.sogya.projects.smartrevolutionapp.screens.home
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.sogya.data.repository.LocalDataBaseRepositoryImpl
+import com.sogya.domain.models.StateDomain
+import com.sogya.domain.usecases.databaseusecase.GetAllStatesUseCase
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
+import ru.sogya.projects.smartrevolutionapp.app.App
 import ru.sogya.projects.smartrevolutionapp.utils.VisibilityStates
 
 class DashboardVM : ViewModel() {
     val loadingViewLiveData = MutableLiveData<Int>()
+    val itemsLiveData = MutableLiveData<List<StateDomain>>()
+    private val repository = LocalDataBaseRepositoryImpl(App.getApplicationContext())
+    private val getStatesUseCase = GetAllStatesUseCase(repository)
 
+    fun getItemsFromLocalDB() {
+        getStatesUseCase.invoke().subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(object : DisposableSingleObserver<List<StateDomain>>() {
+                override fun onSuccess(t: List<StateDomain>) {
+                    itemsLiveData.postValue(t)
+                    loadingViewLiveData.postValue(VisibilityStates.GONE.visibility)
+                }
 
-    init {
-        loadingViewLiveData.postValue(VisibilityStates.GONE.visibility)
+                override fun onError(e: Throwable) {
+                    Log.d("DataBaseGetAllError", e.message.toString())
+                }
+            })
     }
 }
