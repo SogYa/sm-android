c7b6e98a3c30fabd5ef27f89b5d7a44bcc5b71ec
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2022-12-30 11:03:49 +0300

    added delete state dialog

--- app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/home/DashboardFragment.kt
+++ app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/home/DashboardFragment.kt
@@ -1,22 +1,21 @@
 package ru.sogya.projects.smartrevolutionapp.screens.home
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.sogya.domain.models.StateDomain
 import ru.sogya.projects.smartrevolutionapp.R
 import ru.sogya.projects.smartrevolutionapp.databinding.FragmentDashboardBinding
-import ru.sogya.projects.smartrevolutionapp.dialogs.LogOutDialogFragment
+import ru.sogya.projects.smartrevolutionapp.dialogs.DeleteItemDialogFragment
 import ru.sogya.projects.smartrevolutionapp.screens.home.bottomsheet.DashboardBottomSheet
 import ru.sogya.projects.smartrevolutionapp.screens.home.bottomsheet.stateadding.StateAdapter
 
-class DashboardFragment : Fragment(R.layout.fragment_dashboard), StateAdapter.OnStateClickListener {
+class DashboardFragment : Fragment(R.layout.fragment_dashboard), StateAdapter.OnStateClickListener,
+    DeleteItemDialogFragment.DialogFragmentListener {
     private lateinit var binding: FragmentDashboardBinding
     private val vm: DashboardVM by viewModels()
     private lateinit var adapter: StateAdapter
@@ -31,28 +30,42 @@ class DashboardFragment : Fragment(R.layout.fragment_dashboard), StateAdapter.On
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.addButton.setOnClickListener {
-            DashboardBottomSheet().show(childFragmentManager, LogOutDialogFragment().tag)
+            DashboardBottomSheet().show(childFragmentManager, DashboardBottomSheet().tag)
         }
         binding.statesRecyclerView.layoutManager = LinearLayoutManager(context)
         adapter = StateAdapter(this)
         binding.statesRecyclerView.adapter = adapter
-
+        binding.statesRecyclerView.itemAnimator = null
+        binding.swipeRefresh.setOnRefreshListener {
+            vm.getItemsFromLocalDB()
+            binding.swipeRefresh.isRefreshing = false
+        }
     }
 
     override fun onResume() {
         super.onResume()
-        vm.getItemsFromLocalDB()
         vm.loadingViewLiveData.observe(viewLifecycleOwner) {
             binding.loadingView.visibility = it
         }
         vm.itemsLiveData.observe(viewLifecycleOwner) {
             adapter.updateStatesList(it)
-            Log.d("ListStates", it.toString())
         }
     }
 
     override fun onClick(stateDomain: StateDomain) {
-        Toast.makeText(context, "Coming soon..", Toast.LENGTH_SHORT).show()
+        val dialog = DeleteItemDialogFragment(this)
+        val arguments = Bundle()
+        arguments.putString(STATE_ID, stateDomain.entityId)
+        dialog.arguments = arguments
+        dialog.show(childFragmentManager, dialog.tag)
+    }
+
+    override fun positiveButtonClicked(stateId: String) {
+        vm.deleteState(stateId)
+    }
+
+    companion object {
+        private const val STATE_ID = "id"
     }
 
 }
