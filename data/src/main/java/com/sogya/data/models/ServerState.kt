3503e4194b00ad6463dbf29dacc71d8706cf100b
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2022-12-31 13:23:44 +0300

    added serialized name

--- data/src/main/java/com/sogya/data/models/ServerState.kt
+++ data/src/main/java/com/sogya/data/models/ServerState.kt
@@ -2,10 +2,12 @@ package com.sogya.data.models
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
 
 @Entity(tableName = "servers")
 data class ServerState(
     @PrimaryKey(autoGenerate = true)
+    @SerializedName("serverId")
     val serverId: Int,
     val serverName: String,
     val serverUri: String,
