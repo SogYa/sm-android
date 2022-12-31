accb89977ce9f3b7eadb6608f9ec7650895d0ba1
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2022-12-31 13:25:05 +0300

    fixed crash with query; deleted live data

--- data/src/main/java/com/sogya/data/database/ServersDao.kt
+++ data/src/main/java/com/sogya/data/database/ServersDao.kt
@@ -1,23 +1,24 @@
 package com.sogya.data.database
 
 import androidx.lifecycle.LiveData
+import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy.REPLACE
 import androidx.room.Query
 import com.sogya.data.models.ServerState
-
+@Dao
 interface ServersDao {
 
     @Query("SELECT * FROM servers")
     fun getAll(): LiveData<ServerState>
 
-    @Query("SELECT * FROM servers WHERE serverId(:serverId)")
+    @Query("SELECT * FROM servers WHERE serverId IN(:serverId)")
     fun getById(serverId: Int): LiveData<ServerState>
 
     @Insert(onConflict = REPLACE)
-    fun insert(serverState: ServerState): LiveData<Boolean>
+    fun insert(serverState: ServerState)
 
     @Delete
-    fun delete(serverState: ServerState): LiveData<Boolean>
+    fun delete(serverState: ServerState)
 }
