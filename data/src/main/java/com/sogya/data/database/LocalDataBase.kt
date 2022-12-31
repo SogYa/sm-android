4d52fd404228c9c4849e64380c062b2093822111
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2022-12-31 13:26:50 +0300

    added server dao

--- data/src/main/java/com/sogya/data/database/LocalDataBase.kt
+++ data/src/main/java/com/sogya/data/database/LocalDataBase.kt
@@ -2,9 +2,11 @@ package com.sogya.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.sogya.data.models.ServerState
 import com.sogya.data.models.State
 
-@Database(entities = [State::class], version = 1)
+@Database(entities = [State::class, ServerState::class], version = 1)
 abstract class LocalDataBase : RoomDatabase() {
     abstract fun stateDao(): StateDao
+    abstract fun serverDao(): ServersDao
 }
