0afbf2f90e89bbac9c75d68012e6eebf6160c3be
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2023-01-03 13:45:01 +0300

    added repository val; added get server by id method

--- app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/home/DashboardVM.kt
+++ app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/home/DashboardVM.kt
@@ -4,21 +4,31 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.sogya.data.utils.Constants
 import com.sogya.domain.models.StateDomain
+import com.sogya.domain.usecases.databaseusecase.servers.GetServerByIdUseCase
 import com.sogya.domain.usecases.databaseusecase.states.DeleteStateUseCase
 import com.sogya.domain.usecases.databaseusecase.states.GetAllStatesUseCase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import ru.sogya.projects.smartrevolutionapp.app.App
+import ru.sogya.projects.smartrevolutionapp.needtoremove.SPControl
 
 class DashboardVM : ViewModel() {
     val loadingViewLiveData = MutableLiveData<Int>()
     private var itemsLiveData: LiveData<List<StateDomain>> = MutableLiveData()
-    private val getStatesUseCase = GetAllStatesUseCase(App.getRoom())
-    private val deleteUseCase = DeleteStateUseCase(App.getRoom())
+    private val repository = App.getRoom()
+    private val getStatesUseCase = GetAllStatesUseCase(repository)
+    private val deleteUseCase = DeleteStateUseCase(repository)
+    private val getServerByIdUseCase = GetServerByIdUseCase(repository)
 
     init {
         itemsLiveData = getStatesUseCase.invoke()
+        if (SPControl.getInstance().getStringPrefs(Constants.AUTH_TOKEN) == "") {
+            val serverUri = SPControl.getInstance().getStringPrefs(Constants.SERVER_URI)
+            val server = getServerByIdUseCase.invoke(serverUri)
+            SPControl.getInstance().updatePrefs(Constants.AUTH_TOKEN, server.serverToken)
+        }
     }
 
     fun deleteState(stateId: String) {
