7bbfc28874d1c920375c7ee4476a3187eb0309a3
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2023-01-03 13:37:04 +0300

    added constant for server name

--- data/src/main/java/com/sogya/data/utils/Constants.kt
+++ data/src/main/java/com/sogya/data/utils/Constants.kt
@@ -3,8 +3,9 @@ package com.sogya.data.utils
 object Constants {
     const val AUTH_TOKEN = "auth_token"
     const val REDIRECT_URI = "ru.sogya.projects.smartrevolutionapp://"
-    const val URI = "uri"
+    const val SERVER_URI = "uri"
     const val TEST_MODE = "test"
     const val PREFS_IS_LOCKED = "isLocked"
     const val PREFS_APPLOCK_PINCODE = "pincode"
+    const val SERVER_NAME = "name"
 }
