ab449c1978616eccbb0ce9dee02aa3d8de4402c1
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2023-01-06 20:31:46 +0300

    added group dao to database control class

--- data/src/main/java/com/sogya/data/database/LocalDataBase.kt
+++ data/src/main/java/com/sogya/data/database/LocalDataBase.kt
@@ -4,9 +4,11 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.sogya.data.models.ServerState
 import com.sogya.data.models.State
+import com.sogya.data.models.StateGroup
 
-@Database(entities = [State::class, ServerState::class], version = 1)
+@Database(entities = [State::class, ServerState::class, StateGroup::class], version = 1)
 abstract class LocalDataBase : RoomDatabase() {
     abstract fun stateDao(): StateDao
     abstract fun serverDao(): ServersDao
+    abstract fun groupDao(): GroupDao
 }
