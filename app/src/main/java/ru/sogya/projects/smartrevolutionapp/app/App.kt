c06535091ebbc67fbcd6cfc9674fb56954b214ea
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2023-01-15 14:39:02 +0300

    chore

--- app/src/main/java/ru/sogya/projects/smartrevolutionapp/app/App.kt
+++ app/src/main/java/ru/sogya/projects/smartrevolutionapp/app/App.kt
@@ -3,26 +3,27 @@ package ru.sogya.projects.smartrevolutionapp.app
 import android.app.Application
 import android.content.Context
 import com.sogya.data.repository.LocalDataBaseRepositoryImpl
+import com.sogya.domain.repository.LocalDataBaseRepository
 
 class App : Application() {
 
 
     companion object {
         private lateinit var app: App
-        private lateinit var repositoryImpl: LocalDataBaseRepositoryImpl
+        private lateinit var repository: LocalDataBaseRepository
 
         fun getApplicationContext(): Context {
             return app.applicationContext
         }
 
-        fun getRoom(): LocalDataBaseRepositoryImpl {
-            return repositoryImpl
+        fun getRoom(): LocalDataBaseRepository {
+            return repository
         }
     }
 
     override fun onCreate() {
         super.onCreate()
         app = this
-        repositoryImpl = LocalDataBaseRepositoryImpl(app.applicationContext)
+        repository = LocalDataBaseRepositoryImpl(app.applicationContext)
     }
 }
