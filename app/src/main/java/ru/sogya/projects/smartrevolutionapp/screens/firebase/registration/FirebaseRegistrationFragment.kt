d103d4f143935fc0b076bef9a08484713a9b52e5
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2023-01-15 19:43:32 +0300

    added firebase registration screen realization

--- app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/firebase/registration/FirebaseRegistrationFragment.kt
+++ app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/firebase/registration/FirebaseRegistrationFragment.kt
@@ -1,7 +1,58 @@
 package ru.sogya.projects.smartrevolutionapp.screens.firebase.registration
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.sogya.domain.utils.MyCallBack
 import ru.sogya.projects.smartrevolutionapp.R
+import ru.sogya.projects.smartrevolutionapp.databinding.FragmentFirebaseRegistrationBinding
+import ru.sogya.projects.smartrevolutionapp.dialogs.EmailVerificationDialogFragment
 
 class FirebaseRegistrationFragment : Fragment(R.layout.fragment_firebase_registration) {
+    private lateinit var binding: FragmentFirebaseRegistrationBinding
+    private val vm: FirebaseRegistrationVM by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentFirebaseRegistrationBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            binding.buttonSignUp.setOnClickListener {
+                val email = editTextEmail.text.toString()
+                val password = editTextPassword.text.toString()
+                val confirmationPassword = editTextConfirmPassword.text.toString()
+
+                if (password == confirmationPassword && password.length > 6)
+                    vm.signUpUser(email, password, object : MyCallBack<Boolean> {
+                        override fun data(t: Boolean) {
+                            if (t) {
+                                val dialog = EmailVerificationDialogFragment()
+                                dialog.show(childFragmentManager, dialog.tag)
+                            }else
+                                Toast.makeText(context, "Something is wrong with your email", Toast.LENGTH_SHORT).show()
+                        }
+
+                        override fun error() {
+                            Toast.makeText(context, "Something went wrong", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                    })
+                else if (password.length < 6)
+                    Toast.makeText(context, "Password to short", Toast.LENGTH_SHORT).show()
+                else
+                    Toast.makeText(context, "Passwords don't match", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 }
--- app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/firebase/registration/FirebaseRegistrationVM.kt
+++ app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/firebase/registration/FirebaseRegistrationVM.kt
@@ -1,7 +1,39 @@
 package ru.sogya.projects.smartrevolutionapp.screens.firebase.registration
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.sogya.data.repository.FirebaseRepositoryImpl
+import com.sogya.domain.usecases.firebase.CreateUserUseCase
+import com.sogya.domain.usecases.firebase.SendEmailVerivicationUseCase
+import com.sogya.domain.utils.MyCallBack
+import ru.sogya.projects.smartrevolutionapp.utils.VisibilityStates
 
 class FirebaseRegistrationVM : ViewModel() {
+    private val repository = FirebaseRepositoryImpl()
+    private val createUserUseCase = CreateUserUseCase(repository)
+    private val sendEmailVerivicationUseCase = SendEmailVerivicationUseCase(repository)
+    private var loadingLiveData = MutableLiveData<Int>()
 
+    fun signUpUser(email: String, password: String, myCallBack: MyCallBack<Boolean>) {
+        createUserUseCase.invoke(email, password, object : MyCallBack<String> {
+            override fun data(t: String) {
+                sendEmailVerivicationUseCase.invoke(object : MyCallBack<Boolean>{
+                    override fun data(t: Boolean) {
+                        myCallBack.data(true)
+                    }
+
+                    override fun error() {
+                        myCallBack.data(false)
+                    }
+
+                })
+                loadingLiveData.value = VisibilityStates.GONE.visibility
+            }
+
+            override fun error() {
+                myCallBack.error()
+                loadingLiveData.value = VisibilityStates.GONE.visibility
+            }
+        })
+    }
 }
