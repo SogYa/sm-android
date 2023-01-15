3613898a77ca4f62dab381b65d3cb1c598a9e4eb
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2023-01-15 19:42:48 +0300

    added email verify use case

---
+++ domain/src/main/java/com/sogya/domain/usecases/firebase/SendEmailVerivicationUseCase.kt
@@ -0,0 +1,10 @@
+package com.sogya.domain.usecases.firebase
+
+import com.sogya.domain.repository.FirebaseRepository
+import com.sogya.domain.utils.MyCallBack
+
+class SendEmailVerivicationUseCase(
+    private val repository: FirebaseRepository
+) {
+    fun invoke(myCallBack: MyCallBack<Boolean>) = repository.sendEmailVerification(myCallBack)
+}
