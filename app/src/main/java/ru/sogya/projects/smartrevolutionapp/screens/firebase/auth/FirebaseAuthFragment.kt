4ac5695f071928043a868fba64813595e8926e18
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2023-01-15 19:43:22 +0300

    added firebase auth screen realization

--- app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/firebase/auth/FirebaseAuthFragment.kt
+++ app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/firebase/auth/FirebaseAuthFragment.kt
@@ -1,7 +1,50 @@
 package ru.sogya.projects.smartrevolutionapp.screens.firebase.auth
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.sogya.domain.utils.MyCallBack
 import ru.sogya.projects.smartrevolutionapp.R
+import ru.sogya.projects.smartrevolutionapp.databinding.FragmentFirebaseAuthBinding
 
 class FirebaseAuthFragment : Fragment(R.layout.fragment_firebase_auth) {
+    private lateinit var binding: FragmentFirebaseAuthBinding
+    private val vm: FirebaseAuthVM by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentFirebaseAuthBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            buttonLogIn.setOnClickListener {
+                val email = editTextEmail.text.toString()
+                val password = editTextPassword.text.toString()
+                vm.logIn(email, password, object : MyCallBack<Boolean> {
+                    override fun data(t: Boolean) {
+                        Toast.makeText(context, "Welcome", Toast.LENGTH_SHORT).show()
+                    }
+
+                    override fun error() {
+                        Toast.makeText(context, "Authentication failed", Toast.LENGTH_SHORT).show()
+                    }
+
+                })
+            }
+            toRegistration.setOnClickListener {
+                findNavController().navigate(R.id.action_firebaseAuthFragment_to_firebaseRegistrationFragment)
+            }
+        }
+    }
 }
--- app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/firebase/auth/FirebaseAuthVM.kt
+++ app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/firebase/auth/FirebaseAuthVM.kt
@@ -1,6 +1,28 @@
 package ru.sogya.projects.smartrevolutionapp.screens.firebase.auth
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.sogya.data.repository.FirebaseRepositoryImpl
+import com.sogya.domain.usecases.firebase.LogInUserUseCase
+import com.sogya.domain.utils.MyCallBack
+import ru.sogya.projects.smartrevolutionapp.utils.VisibilityStates
 
 class FirebaseAuthVM : ViewModel() {
+    private val repository = FirebaseRepositoryImpl()
+    private val logInUserUseCase = LogInUserUseCase(repository)
+    private var loadingLiveData = MutableLiveData<Int>()
+
+    fun logIn(email: String, password: String, myCallBack: MyCallBack<Boolean>) {
+        logInUserUseCase.invoke(email, password, object : MyCallBack<String> {
+            override fun data(t: String) {
+                myCallBack.data(true)
+                loadingLiveData.value = VisibilityStates.GONE.visibility
+            }
+
+            override fun error() {
+                myCallBack.error()
+                loadingLiveData.value = VisibilityStates.GONE.visibility
+            }
+        })
+    }
 }
