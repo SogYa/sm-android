8eb7a8183cbccd4860abac9c7878d5401a73778e
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2023-01-15 14:39:58 +0300

    created fragment for firebase auth

---
+++ app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/firebase/auth/FirebaseAuthFragment.kt
@@ -0,0 +1,7 @@
+package ru.sogya.projects.smartrevolutionapp.screens.firebase.auth
+
+import androidx.fragment.app.Fragment
+import ru.sogya.projects.smartrevolutionapp.R
+
+class FirebaseAuthFragment : Fragment(R.layout.fragment_firebase_auth) {
+}
---
+++ app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/firebase/auth/FirebaseAuthVM.kt
@@ -0,0 +1,6 @@
+package ru.sogya.projects.smartrevolutionapp.screens.firebase.auth
+
+import androidx.lifecycle.ViewModel
+
+class FirebaseAuthVM : ViewModel() {
+}
