783230fa53344a68bcf5d8ca0e28762b64bba7dd
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2023-01-23 18:27:53 +0300

    added firebase user models

---
+++ data/src/main/java/com/sogya/data/models/UserData.kt
@@ -0,0 +1,8 @@
+package com.sogya.data.models
+
+data class UserData(
+    val id: String,
+    val email: String,
+    val name: String,
+    val serverUriList: List<String>
+)
---
+++ domain/src/main/java/com/sogya/domain/models/UserDomain.kt
@@ -0,0 +1,8 @@
+package com.sogya.domain.models
+
+data class UserDomain(
+    val id: String,
+    val email: String,
+    val name: String,
+    val serverUriList: List<String>
+)
