51c9fd624b12a788dace06bb8eed89892a6ce9d2
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2023-01-24 22:09:07 +0300

    chore

--- data/src/main/java/com/sogya/data/models/UserData.kt
+++ data/src/main/java/com/sogya/data/models/UserData.kt
@@ -1,8 +1,8 @@
 package com.sogya.data.models
 
 data class UserData(
-    val id: String,
-    val email: String,
-    val name: String,
-    val serverUriList: List<String>
+    val id: String = "",
+    val email: String = "",
+    val name: String = "",
+    val serverUriList: List<String>? = listOf()
 )
--- domain/src/main/java/com/sogya/domain/models/UserDomain.kt
+++ domain/src/main/java/com/sogya/domain/models/UserDomain.kt
@@ -1,8 +1,8 @@
 package com.sogya.domain.models
 
 data class UserDomain(
-    val id: String,
-    val email: String,
-    val name: String,
-    val serverUriList: List<String>
+    val id: String = "",
+    val email: String = "",
+    val name: String = "",
+    val serverUriList: List<String>? = listOf()
 )
