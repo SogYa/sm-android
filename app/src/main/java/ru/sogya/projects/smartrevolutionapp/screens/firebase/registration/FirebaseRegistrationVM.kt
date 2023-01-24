3897abd843d1cf86c2a39c91d5ecb36deb8e9b04
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2023-01-24 22:11:20 +0300

    reworked

--- app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/firebase/registration/FirebaseRegistrationVM.kt
+++ app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/firebase/registration/FirebaseRegistrationVM.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.sogya.domain.usecases.firebase.CreateUserUseCase
 import com.sogya.domain.usecases.firebase.SendEmailVerivicationUseCase
+import com.sogya.domain.usecases.firebase.WriteUserUseCase
 import com.sogya.domain.utils.Constants
 import com.sogya.domain.utils.MyCallBack
 import ru.sogya.projects.smartrevolutionapp.app.App
@@ -12,32 +13,44 @@ import ru.sogya.projects.smartrevolutionapp.needtoremove.SPControl
 import ru.sogya.projects.smartrevolutionapp.utils.VisibilityStates
 
 class FirebaseRegistrationVM : ViewModel() {
-    private val createUserUseCase = CreateUserUseCase(App.getFirebase())
-    private val sendEmailVerivicationUseCase = SendEmailVerivicationUseCase(App.getFirebase())
+    private val repository = App.getFirebase()
+    private val createUserUseCase = CreateUserUseCase(repository)
+    private val sendEmailVerificationUseCase = SendEmailVerivicationUseCase(App.getFirebase())
+    private val writeUserUseCase = WriteUserUseCase(repository)
+    private val errorLiveData = MutableLiveData<String>()
     private var loadingLiveData = MutableLiveData<Int>()
 
-    fun signUpUser(email: String, password: String, myCallBack: MyCallBack<Boolean>) {
+    fun signUpUser(name: String, email: String, password: String, myCallBack: MyCallBack<Boolean>) {
         createUserUseCase.invoke(email, password, object : MyCallBack<String> {
             override fun data(t: String) {
-                sendEmailVerivicationUseCase.invoke(object : MyCallBack<Boolean> {
-                    override fun data(t: Boolean) {
+                writeUserUseCase.invoke(name, email, object : MyCallBack<String> {
+                    override fun error(string: String) {
+                        errorLiveData.value = string
+                    }
+                })
+                sendEmailVerificationUseCase.invoke(object : MyCallBack<String> {
+                    override fun data(t: String) {
                         SPControl.getInstance().updatePrefs(Constants.IS_FIREBASE_AUTH, true)
                         myCallBack.data(true)
                     }
 
-                    override fun error() {
-                        myCallBack.data(false)
-                        Log.d("Firebase","error")
+                    override fun error(string: String) {
+                        errorLiveData.value = string
+                        Log.d("Firebase", "error")
                     }
 
                 })
                 loadingLiveData.value = VisibilityStates.GONE.visibility
             }
 
-            override fun error() {
-                myCallBack.error()
+            override fun error(string: String) {
+                myCallBack.error(string)
                 loadingLiveData.value = VisibilityStates.GONE.visibility
             }
         })
     }
+
+    fun getLoadingLiveData() = loadingLiveData
+
+    fun getErrorLiveData() = errorLiveData
 }
