fb4d6916f8c275c1f6e1596b49a05f8a5b11e803
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2023-01-24 22:09:58 +0300

    added server worker class

---
+++ app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/servers/ServerWorker.kt
@@ -0,0 +1,14 @@
+package ru.sogya.projects.smartrevolutionapp.screens.servers
+
+import android.content.Context
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import ru.sogya.projects.smartrevolutionapp.app.App
+
+class ServerWorker(context: Context, workerParams: WorkerParameters) :
+    Worker(context, workerParams) {
+    private val repository = App.getWebSocketRepository()
+    override fun doWork(): Result {
+        TODO("Not yet implemented")
+    }
+}
