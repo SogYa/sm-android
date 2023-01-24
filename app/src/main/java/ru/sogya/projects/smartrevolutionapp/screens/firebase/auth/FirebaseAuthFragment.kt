b5def47e5457a94330f0ad443469fb919f005a3a
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2023-01-24 22:10:29 +0300

    added null check

--- app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/firebase/auth/FirebaseAuthFragment.kt
+++ app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/firebase/auth/FirebaseAuthFragment.kt
@@ -3,6 +3,7 @@ package ru.sogya.projects.smartrevolutionapp.screens.firebase.auth
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -25,23 +26,37 @@ class FirebaseAuthFragment : Fragment(R.layout.fragment_firebase_auth) {
         return binding.root
     }
 
+    override fun onStart() {
+        super.onStart()
+        vm.getLoadingLiveData().observe(viewLifecycleOwner) {
+            binding.loadingView.visibility = it
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.apply {
             buttonLogIn.setOnClickListener {
                 val email = editTextEmail.text.toString()
                 val password = editTextPassword.text.toString()
-                vm.logIn(email, password, object : MyCallBack<Boolean> {
-                    override fun data(t: Boolean) {
-                        findNavController().navigate(R.id.action_firebaseAuthFragment_to_serversFragment)
-                        Toast.makeText(context, "Welcome", Toast.LENGTH_SHORT).show()
-                    }
+                if (email != "" || password != "") {
+                    loadingView.visibility = VISIBLE
+                    vm.logIn(email, password, object : MyCallBack<Boolean> {
+                        override fun data(t: Boolean) {
+                            findNavController().navigate(R.id.action_firebaseAuthFragment_to_serversFragment)
+                            Toast.makeText(context, "Welcome", Toast.LENGTH_SHORT).show()
+                        }
 
-                    override fun error() {
-                        Toast.makeText(context, "Authentication failed", Toast.LENGTH_SHORT).show()
-                    }
+                        override fun error() {
+                            Toast.makeText(context, "Authentication failed", Toast.LENGTH_SHORT)
+                                .show()
+                        }
 
-                })
+                    })
+                } else {
+                    Toast.makeText(context, "Some fields are not filled in", Toast.LENGTH_SHORT)
+                        .show()
+                }
             }
             toRegistration.setOnClickListener {
                 findNavController().navigate(R.id.action_firebaseAuthFragment_to_firebaseRegistrationFragment)
