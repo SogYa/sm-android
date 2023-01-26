7940d4e29c62c534761ab710a13ff583020be91f
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2023-01-26 21:54:27 +0300

    added ticket screen

---
+++ app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/ticket/ticket/TicketFragment.kt
@@ -0,0 +1,7 @@
+package ru.sogya.projects.smartrevolutionapp.screens.ticket.ticket
+
+import androidx.fragment.app.Fragment
+import ru.sogya.projects.smartrevolutionapp.R
+
+class TicketFragment : Fragment(R.layout.fragment_ticket) {
+}
---
+++ app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/ticket/ticket/TicketVM.kt
@@ -0,0 +1,6 @@
+package ru.sogya.projects.smartrevolutionapp.screens.ticket.ticket
+
+import androidx.lifecycle.ViewModel
+
+class TicketVM : ViewModel() {
+}
