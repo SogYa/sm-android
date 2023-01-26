596de3fdcacc3cbb2bf12b7944487cc876930063
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2023-01-26 21:55:12 +0300

    added ticket creation screen

---
+++ app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/ticket/create/CreateTicketFragment.kt
@@ -0,0 +1,7 @@
+package ru.sogya.projects.smartrevolutionapp.screens.ticket.create
+
+import androidx.fragment.app.Fragment
+import ru.sogya.projects.smartrevolutionapp.R
+
+class CreateTicketFragment: Fragment(R.layout.fragment_create_ticket) {
+}
