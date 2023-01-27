14c73c1600616b1c2fc1c06c85416d53127843df
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2023-01-27 13:53:28 +0300

    added new fields

--- data/src/main/java/com/sogya/data/models/TicketData.kt
+++ data/src/main/java/com/sogya/data/models/TicketData.kt
@@ -2,8 +2,10 @@ package com.sogya.data.models
 
 data class TicketData(
     val ticketId: String = "",
+    val userId: String = "",
     val ticketDate: String = "",
     val ticketDevice: String = "",
     val ticketZone: String = "",
-    val ticketDesc: String? = ""
+    val ticketDesc: String? = "",
+    val ticketStatus: String = ""
 )
--- domain/src/main/java/com/sogya/domain/models/TicketDomain.kt
+++ domain/src/main/java/com/sogya/domain/models/TicketDomain.kt
@@ -1,9 +1,11 @@
 package com.sogya.domain.models
 
 data class TicketDomain(
-    val ticketId:String ="",
+    val ticketId: String = "",
+    val userId: String = "",
     val ticketDate: String = "",
-    val ticketDevice:String = "",
-    val ticketZone:String ="",
-    val ticketDesc: String? = ""
+    val ticketDevice: String = "",
+    val ticketZone: String = "",
+    val ticketDesc: String? = "",
+    val ticketStatus: String = ""
 )
