e43ed0b96ebc60af7bf36d76b97e7f189b001c3e
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2023-01-27 16:58:15 +0300

    added ticket screen realization

--- app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/ticket/ticket/TicketFragment.kt
+++ app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/ticket/ticket/TicketFragment.kt
@@ -1,15 +1,22 @@
 package ru.sogya.projects.smartrevolutionapp.screens.ticket.ticket
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.VISIBLE
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.sogya.domain.utils.Constants
+import com.sogya.domain.utils.MyCallBack
 import ru.sogya.projects.smartrevolutionapp.R
 import ru.sogya.projects.smartrevolutionapp.databinding.FragmentTicketBinding
 
 class TicketFragment : Fragment(R.layout.fragment_ticket) {
     private lateinit var binding: FragmentTicketBinding
+    private val vm: TicketVM by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,5 +25,52 @@ class TicketFragment : Fragment(R.layout.fragment_ticket) {
     ): View {
         binding = FragmentTicketBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    companion object {
+        private const val COLOR_YELLOW = "#FFFF00"
+        private const val COLOR_RED = "#FF0000"
+        private const val COLOR_GREEN = "#00FF00"
+    }
+
+    override fun onStart() {
+        super.onStart()
+        vm.getLoadingLiveData().observe(viewLifecycleOwner) {
+            binding.loadingView.visibility = it
+        }
+        vm.getTicketLiveData().observe(viewLifecycleOwner) {
+            if (it.ticketDesc != " ") {
+                binding.descriptionBlock.visibility = VISIBLE
+                binding.textViewTicketDesc.text = it.ticketDesc
+            }
+            binding.textViewTicketId.text = it.ticketId
+            binding.textViewTicketDevice.text = it.ticketDevice
+            binding.textViewTicketZone.text = it.ticketZone
+            binding.textViewTicketStatus.text = it.ticketStatus
+            when (it.ticketStatus) {
+                "Created" -> {
+                    binding.textViewTicketStatus.setTextColor(Color.parseColor(COLOR_YELLOW))
+                }
+                "Done" -> {
+                    binding.textViewTicketStatus.setTextColor(Color.parseColor(COLOR_GREEN))
+                }
+                "Canceled" -> {
+                    binding.textViewTicketStatus.setTextColor(Color.parseColor(COLOR_RED))
+                }
+            }
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val ticketId = arguments?.getString(Constants.TICKET_ID_KEY)
+        ticketId?.let {
+            vm.getTicket(it, object : MyCallBack<Boolean> {
+                override fun error(error: String) {
+                    super.error(error)
+                    Toast.makeText(context, error, Toast.LENGTH_SHORT).show()
+                }
+            })
+        }
     }
 }
--- app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/ticket/ticket/TicketVM.kt
+++ app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/ticket/ticket/TicketVM.kt
@@ -1,6 +1,35 @@
 package ru.sogya.projects.smartrevolutionapp.screens.ticket.ticket
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.sogya.domain.models.TicketDomain
+import com.sogya.domain.usecases.firebase.ticket.ReadTicketByIdUseCase
+import com.sogya.domain.utils.MyCallBack
+import ru.sogya.projects.smartrevolutionapp.app.App
+import ru.sogya.projects.smartrevolutionapp.utils.VisibilityStates
 
 class TicketVM : ViewModel() {
+    private val repository = App.getFirebase()
+    private val getTicketUseCase = ReadTicketByIdUseCase(repository)
+    private val loadingLiveData = MutableLiveData<Int>()
+    private val ticketLiveData = MutableLiveData<TicketDomain>()
+
+    fun getTicket(ticketId: String, myCallBack: MyCallBack<Boolean>) {
+        loadingLiveData.value = VisibilityStates.VISIBLE.visibility
+        getTicketUseCase.invoke(ticketId, object : MyCallBack<TicketDomain> {
+            override fun data(t: TicketDomain) {
+                super.data(t)
+                loadingLiveData.postValue(VisibilityStates.GONE.visibility)
+                ticketLiveData.postValue(t)
+            }
+
+            override fun error(error: String) {
+                super.error(error)
+                myCallBack.error(error)
+            }
+        })
+    }
+
+    fun getTicketLiveData() = ticketLiveData
+    fun getLoadingLiveData() = loadingLiveData
 }
