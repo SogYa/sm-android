2540952c9a10b66854fa5aa65c21c5e237228ee1
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2023-01-27 16:58:09 +0300

    added create ticket screen realization

--- app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/ticket/create/CreateTicketFragment.kt
+++ app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/ticket/create/CreateTicketFragment.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.sogya.domain.utils.MyCallBack
 import ru.sogya.projects.smartrevolutionapp.R
 import ru.sogya.projects.smartrevolutionapp.databinding.FragmentCreateTicketBinding
+import ru.sogya.projects.smartrevolutionapp.dialogs.TicketCreatedDialogFragment
 
 class CreateTicketFragment : Fragment(R.layout.fragment_create_ticket) {
     private lateinit var binding: FragmentCreateTicketBinding
-
+    private val vm: CreateTicketVM by viewModels()
+    private val deviceList = arrayListOf("Camera", "Door", "Chair", "Electric cat")
+    private val zoneList = arrayListOf("Bedroom", "Kitchen", "Dungeon gym")
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,5 +27,55 @@ class CreateTicketFragment : Fragment(R.layout.fragment_create_ticket) {
     ): View {
         binding = FragmentCreateTicketBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        vm.getLoadingLiveData().observe(viewLifecycleOwner) {
+            binding.loadingView.visibility = it
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val spinnerDeviceAdapter =
+            ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, deviceList)
+        spinnerDeviceAdapter.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item)
+        val spinnerZonesAdapter =
+            ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, zoneList)
+        spinnerZonesAdapter.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item)
+        binding.spinnerDevices.adapter = spinnerDeviceAdapter
+        binding.spinnerZones.adapter = spinnerZonesAdapter
+
+        binding.buttonCreateTicket.setOnClickListener {
+            val device = binding.spinnerDevices.selectedItem.toString()
+            val zone = binding.spinnerZones.selectedItem.toString()
+            val description: String = binding.editTextDescription.text.toString()
+
+            if (device != "" && zone != "") {
+                vm.createTicket(
+                    device = device,
+                    zone = zone,
+                    description = description,
+                    object : MyCallBack<Boolean>, TicketCreatedDialogFragment.TicketClickListener {
+                        override fun data(t: Boolean) {
+                            super.data(t)
+                            val dialog = TicketCreatedDialogFragment(this)
+                            dialog.show(childFragmentManager, dialog.tag)
+                        }
+
+                        override fun error(error: String) {
+                            super.error(error)
+                            Toast.makeText(context, error, Toast.LENGTH_SHORT).show()
+                        }
+
+                        override fun onCloseClick() {
+                            findNavController().popBackStack()
+                        }
+                    })
+            } else {
+                Toast.makeText(context, "Some fields are not filled in", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
--- app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/ticket/create/CreateTicketVM.kt
+++ app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/ticket/create/CreateTicketVM.kt
@@ -1,6 +1,47 @@
 package ru.sogya.projects.smartrevolutionapp.screens.ticket.create
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.sogya.domain.usecases.firebase.ticket.CreateTicketUseCase
+import com.sogya.domain.utils.MyCallBack
+import ru.sogya.projects.smartrevolutionapp.app.App
+import ru.sogya.projects.smartrevolutionapp.utils.VisibilityStates
+import java.text.SimpleDateFormat
+import java.util.*
 
 class CreateTicketVM : ViewModel() {
+    private val repository = App.getFirebase()
+    private val createTicketUseCase = CreateTicketUseCase(repository)
+    private val loadingLiveData = MutableLiveData<Int>()
+
+    fun createTicket(
+        device: String,
+        zone: String,
+        description: String?,
+        myCallBack: MyCallBack<Boolean>
+    ) {
+        val time = Calendar.getInstance().time
+        val dateFormat = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault())
+        val date = dateFormat.format(time)
+        loadingLiveData.postValue(VisibilityStates.VISIBLE.visibility)
+        createTicketUseCase.invoke(
+            ticketDevice = device,
+            ticketZone = zone,
+            ticketDesc = description,
+            ticketDate = date,
+            object : MyCallBack<Boolean> {
+                override fun data(t: Boolean) {
+                    super.data(t)
+                    loadingLiveData.postValue(VisibilityStates.GONE.visibility)
+                    myCallBack.data(true)
+                }
+
+                override fun error(error: String) {
+                    super.error(error)
+                    myCallBack.error(error)
+                }
+            })
+    }
+
+    fun getLoadingLiveData() = loadingLiveData
 }
