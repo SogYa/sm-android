0d215e635c7332d005ff4540d0be7b6634a9f9b9
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2023-02-02 11:08:00 +0300

    added all new view holders for different layouts

--- app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/home/bottomsheet/stateadding/StateAdapter.kt
+++ app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/home/bottomsheet/stateadding/StateAdapter.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.widget.SwitchCompat
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.sogya.domain.models.StateDomain
@@ -20,6 +21,9 @@ class StateAdapter(
     companion object {
         private const val LIGHT_PINK = "#FFE0EB"
         private const val IS_SENSOR = 0
+        private const val IS_SUN = 1
+        private const val IS_USER = 2
+        private const val IS_SWITCH = 3
     }
 
     class SensorWeatherViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -29,6 +33,23 @@ class StateAdapter(
         val iconView: ImageView = itemView.findViewById(R.id.imageViewIcon)
     }
 
+    class SunViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val texViewLabel: TextView = itemView.findViewById(R.id.textViewLable)
+        val textViewState: TextView = itemView.findViewById(R.id.textViewState)
+        val iconView: ImageView = itemView.findViewById(R.id.imageViewIcon)
+    }
+
+    class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val texViewLabel: TextView = itemView.findViewById(R.id.textViewUserLable)
+        val iconView: ImageView = itemView.findViewById(R.id.imageViewUserIcon)
+    }
+
+    class SwitchViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val texViewLabel: TextView = itemView.findViewById(R.id.textViewSwitchLable)
+        val textViewId: TextView = itemView.findViewById(R.id.textViewSwitchId)
+        val switchState: SwitchCompat = itemView.findViewById(R.id.switchState)
+
+    }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val idTextView: TextView = itemView.findViewById(R.id.textId)
@@ -36,44 +57,105 @@ class StateAdapter(
         val lastChangeTV: TextView = itemView.findViewById(R.id.textLastChanged)
     }
 
+
     override fun getItemViewType(position: Int): Int {
-        if (states[position].entityId.startsWith("sensor.")) {
+        val entityId = states[position].entityId
+        if (entityId.startsWith("sensor.")) {
             return IS_SENSOR
+        } else if (entityId.startsWith("sun.")) {
+            return IS_SUN
+        } else if (entityId.startsWith("person."))
+            return IS_USER
+        else if (entityId.startsWith("switch.")) {
+            return IS_SWITCH
         }
         return -1
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val view: View
-        if (viewType == IS_SENSOR) {
-            view = LayoutInflater.from(parent.context)
-                .inflate(R.layout.state_sensor_weather_item, parent, false)
-            return SensorWeatherViewHolder(view)
-        } else {
-            view = LayoutInflater.from(parent.context)
-                .inflate(R.layout.state_default_item, parent, false)
-            return ViewHolder(view)
+        when (viewType) {
+            IS_SENSOR -> {
+                view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.state_sensor_weather_item, parent, false)
+                return SensorWeatherViewHolder(view)
+            }
+            IS_SUN -> {
+                view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.state_sun_item, parent, false)
+                return SunViewHolder(view)
+            }
+            IS_USER -> {
+                view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.state_user_item, parent, false)
+                return UserViewHolder(view)
+            }
+            IS_SWITCH -> {
+                view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.state_switch_item, parent, false)
+                return SwitchViewHolder(view)
+            }
+            else -> {
+                view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.state_default_item, parent, false)
+                return ViewHolder(view)
+            }
         }
-
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val stateDomain: StateDomain = states[position]
-        if (holder is SensorWeatherViewHolder) {
-            if (stateDomain.attributesDomain?.deviceClass == "temperature") {
-                holder.iconView.setImageResource(R.drawable.temperature_thermometer_cold_warm_hot_icon_194210)
-                holder.textViewState.text = "${stateDomain.state}°C"
-            } else if (stateDomain.attributesDomain?.deviceClass == "humidity") {
-                holder.iconView.setImageResource(R.drawable.humidity_icon_216457)
-                holder.textViewState.text = "${stateDomain.state}%"
-            }
-            holder.texViewLabel.text = stateDomain.attributesDomain?.friendlyName
-            holder.itTextView.text = stateDomain.entityId
+        when (holder) {
+            is SensorWeatherViewHolder -> {
+                when (stateDomain.attributesDomain?.deviceClass) {
+                    "temperature" -> {
+                        holder.textViewState.text = "${stateDomain.state}°C"
+                        holder.iconView.setImageResource(R.drawable.ic_thermometer)
+                    }
+                    "humidity" -> {
+                        holder.textViewState.text = "${stateDomain.state}%"
+                        holder.iconView.setImageResource(R.drawable.ic_thermometer)
+                    }
+                    else -> {
+                        holder.textViewState.text = stateDomain.state
+                    }
+                }
+                holder.texViewLabel.text = stateDomain.attributesDomain?.friendlyName
+                holder.itTextView.text = stateDomain.entityId
 
-        } else if (holder is ViewHolder) {
-            holder.idTextView.text = stateDomain.attributesDomain!!.friendlyName
-            holder.stateTV.text = stateDomain.state
-            holder.lastChangeTV.text = stateDomain.lastChanged
+            }
+            is SunViewHolder -> {
+                if (stateDomain.state == "above_horizon") {
+                    holder.iconView.setImageResource(R.drawable.ic_sun)
+                    holder.textViewState.text = "Above the horizon"
+                } else {
+                    holder.iconView.setImageResource(R.drawable.ic_moon)
+                    holder.textViewState.text = "Below the horizon"
+                }
+                holder.texViewLabel.text = stateDomain.attributesDomain?.friendlyName
+            }
+            is UserViewHolder -> {
+                holder.texViewLabel.text = stateDomain.attributesDomain?.friendlyName
+                holder.iconView.setImageResource(R.drawable.ic_person)
+            }
+            is SwitchViewHolder -> {
+                holder.texViewLabel.text = stateDomain.attributesDomain?.friendlyName
+                holder.textViewId.text = stateDomain.entityId
+                holder.switchState.text = stateDomain.state
+                holder.switchState.isChecked = stateDomain.state == "on"
+                holder.switchState.setOnCheckedChangeListener { _, isChecked ->
+                    val state = if (isChecked) {
+                        "turn_on"
+                    } else
+                        "turn_off"
+                    onStateClickListener?.onSwitchStateChanged(stateDomain.entityId, state)
+                }
+            }
+            is ViewHolder -> {
+                holder.idTextView.text = stateDomain.attributesDomain!!.friendlyName
+                holder.stateTV.text = stateDomain.state
+                holder.lastChangeTV.text = stateDomain.lastChanged
+            }
         }
         holder.itemView.setOnLongClickListener {
             val card = holder.itemView.findViewById<CardView>(R.id.cardItem)
@@ -108,5 +190,6 @@ class StateAdapter(
 
     interface OnStateClickListener {
         fun onClick(stateDomain: StateDomain)
+        fun onSwitchStateChanged(stateId: String, switchState: String) {}
     }
 }
