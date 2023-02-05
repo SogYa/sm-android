a0f85fdd9ddbbd47a17ecf19a13c2bb56fd68967
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2023-02-05 17:35:19 +0300

    added device entities

---
+++ data/src/main/java/com/sogya/data/models/DeviceData.kt
@@ -0,0 +1,27 @@
+package com.sogya.data.models
+
+import com.google.gson.annotations.SerializedName
+import com.sogya.domain.utils.Constants
+
+data class DeviceData(
+    @SerializedName("device_id")
+    val deviceId: String = "SmartRevolutionAndroid_" + System.currentTimeMillis(),
+    @SerializedName("app_id")
+    val appId: String = "SmartRevolutionApp",
+    @SerializedName("app_name")
+    val appName: String = "Smart Revolution",
+    @SerializedName("app_version")
+    val appVersion: String = Constants.APP_VERSION,
+    @SerializedName("device_name")
+    var deviceName: String,
+    @SerializedName("manufacturer")
+    var manufacturer: String,
+    @SerializedName("model")
+    var model: String,
+    @SerializedName("os_name")
+    val operationSystemName: String = "Android",
+    @SerializedName("os_version")
+    var operationSystemVersion: String,
+    @SerializedName("supports_encryption")
+    val support: String = ""
+)
