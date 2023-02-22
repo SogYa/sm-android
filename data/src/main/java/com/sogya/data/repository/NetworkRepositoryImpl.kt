ee5258bcc9a45fcfc8d67c52dbda46c932382e54
Timofey <Ghost.Spartan36@gmail.com>
2023-02-22 15:17:32 +0300

    added send integration method

--- data/src/main/java/com/sogya/data/repository/NetworkRepositoryImpl.kt
+++ data/src/main/java/com/sogya/data/repository/NetworkRepositoryImpl.kt
@@ -1,15 +1,15 @@
 package com.sogya.data.repository
 
-import com.sogya.data.mappers.state.ListOfStatesMapper
 import com.sogya.data.mappers.message.MessageMapper
+import com.sogya.data.mappers.state.ListOfStatesMapper
+import com.sogya.data.models.requests.IntegrationRequestData
 import com.sogya.data.network.api.NetworkService
-import com.sogya.domain.models.Message
-import com.sogya.domain.models.StateDomain
-import com.sogya.domain.models.TokenInfo
+import com.sogya.domain.models.*
 import com.sogya.domain.repository.NetworkRepository
 import io.reactivex.Single
 
 class NetworkRepositoryImpl : NetworkRepository {
+
     override fun getMessage(baseUri: String, token: String): Single<Message> {
         return NetworkService.getRetrofitService(baseUri).getApiMessage(token).map {
             return@map MessageMapper(it).toMessageDomian()
@@ -26,5 +26,25 @@ class NetworkRepositoryImpl : NetworkRepository {
             .getApiStates(token).map {
                 return@map ListOfStatesMapper(it).toDomainList()
             }
+    }
+
+    override fun sendAppIntegration(
+        baseUri: String,
+        token: String,
+        deviceData: DeviceDataDomain
+    ): Single<IntegrationResponseDomain> {
+        return NetworkService.getRetrofitService(baseUri).registrationMobileAppIntegration(
+            token,
+            IntegrationRequestData(
+                deviceId = deviceData.deviceId,
+                deviceName = deviceData.deviceName,
+                manufacturer = deviceData.manufacturer,
+                model = deviceData.model,
+                operationSystemName = deviceData.operationSystemName,
+                operationSystemVersion = deviceData.operationSystemVersion
+            )
+        ).map {
+            return@map it.toDomain()
+        }
     }
 }
--- domain/src/main/java/com/sogya/domain/repository/NetworkRepository.kt
+++ domain/src/main/java/com/sogya/domain/repository/NetworkRepository.kt
@@ -1,13 +1,15 @@
 package com.sogya.domain.repository
 
-import com.sogya.domain.models.Message
-import com.sogya.domain.models.StateDomain
-import com.sogya.domain.models.TokenInfo
+import com.sogya.domain.models.*
 import io.reactivex.Single
 
 interface NetworkRepository {
+    fun getMessage(baseUri: String,token: String): Single<Message>
+    fun getToken(baseUri: String,authCode: String): Single<TokenInfo>
+    fun getStates(baseUri: String,token: String): Single<List<StateDomain>>
 
-    fun getMessage(baseUri: String, token: String): Single<Message>
-    fun getToken(baseUri: String, authCode: String): Single<TokenInfo>
-    fun getStates(baseUri: String, token: String): Single<List<StateDomain>>
+    fun sendAppIntegration(baseUri: String,
+        token: String,
+        deviceData: DeviceDataDomain
+    ): Single<IntegrationResponseDomain>
 }
