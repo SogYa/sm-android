a64506b16097914cf00b901a57a52430e8e6dc23
Timofey <Ghost.Spartan36@gmail.com>
2023-02-22 15:18:20 +0300

    added send integration method realization

--- app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/autharization/AuthorizationVM.kt
+++ app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/autharization/AuthorizationVM.kt
@@ -1,17 +1,20 @@
 package ru.sogya.projects.smartrevolutionapp.screens.autharization
 
+import android.os.Build
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.sogya.data.models.requests.AuthMessage
 import com.sogya.data.models.requests.LongLivedRequest
-import com.sogya.data.repository.NetworkRepositoryImpl
+import com.sogya.domain.models.DeviceDataDomain
+import com.sogya.domain.models.IntegrationResponseDomain
 import com.sogya.domain.models.ServerStateDomain
 import com.sogya.domain.models.TokenInfo
 import com.sogya.domain.repository.MessageListener
-import com.sogya.domain.usecases.GetTokenUseCase
 import com.sogya.domain.usecases.databaseusecase.servers.InsertServerUseCase
+import com.sogya.domain.usecases.network.GetTokenUseCase
+import com.sogya.domain.usecases.network.SendAppIntegrationUseCase
 import com.sogya.domain.usecases.sharedpreferences.UpdatePrefsUseCase
 import com.sogya.domain.usecases.websocketus.CloseUseCase
 import com.sogya.domain.usecases.websocketus.InitUseCase
@@ -28,11 +31,12 @@ import kotlin.concurrent.thread
 
 
 class AuthorizationVM : ViewModel(), MessageListener {
-    private val networkRepository = NetworkRepositoryImpl()
+    private val networkRepository = App.getNetworkRepository()
     private val webSocketRepository = App.getWebSocketRepository()
     private val sharedPreferencesRepository = App.getSharedPreferncesRepository()
     private val updatePrefsUseCase = UpdatePrefsUseCase(sharedPreferencesRepository)
     private val getTokenUseCase = GetTokenUseCase(networkRepository)
+    private val sendAppIntegrationUseCase = SendAppIntegrationUseCase(networkRepository)
     private val initUseCase = InitUseCase(webSocketRepository)
     private val sendMessageUseCase = SendMessageUseCase(webSocketRepository)
     private val insertServerUseCase = InsertServerUseCase(App.getRoom())
@@ -84,6 +88,15 @@ class AuthorizationVM : ViewModel(), MessageListener {
         updatePrefsUseCase.invoke(Constants.TEST_MODE, true)
     }
 
+    fun getDeviceData() = DeviceDataDomain(
+        Build.ID,
+        Build.DEVICE,
+        Build.MANUFACTURER,
+        Build.MODEL,
+        Build.VERSION.CODENAME,
+        Build.VERSION.RELEASE
+    )
+
     override fun onConnectSuccess() {
         Log.d("WEBSUCCES", "Connected")
         loadScreenLiveData.postValue(VisibilityStates.VISIBLE.visibility)
@@ -107,8 +120,10 @@ class AuthorizationVM : ViewModel(), MessageListener {
         val result = JSONObject(text.toString())
         if (result.get("type") == "auth_ok") {
             sendMessageUseCase.invoke(LongLivedRequest())
+
         } else if (result.get("type") == "result") {
             serverToken = result.get("result").toString()
+            invokeIntegration()
             updatePrefsUseCase.invoke(Constants.AUTH_TOKEN, serverToken)
             insertServerUseCase.invoke(
                 serverStateDomain = ServerStateDomain(
@@ -120,6 +135,22 @@ class AuthorizationVM : ViewModel(), MessageListener {
             navigationLiveData.postValue(true)
             webSocketRepository.close()
         }
+    }
+
+    private fun invokeIntegration() {
+        sendAppIntegrationUseCase.invoke(serverUri,serverToken, getDeviceData()).subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(object : DisposableSingleObserver<IntegrationResponseDomain>() {
+                override fun onSuccess(t: IntegrationResponseDomain) {
+                    updatePrefsUseCase.invoke(Constants.INTEGRATION_SECRET, t.token)
+                    updatePrefsUseCase.invoke(Constants.INTEGRATION_WEB_HOOK, t.webhookId)
+                }
+
+                override fun onError(e: Throwable) {
+                    Log.d("IntegrationError", e.message.toString())
+                }
+
+            })
     }
 
     fun getLoadingLiveData(): LiveData<Int> = loadScreenLiveData
