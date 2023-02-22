3f84bb1dd8259a18733cc74443ca67b91df3d807
Timofey <Ghost.Spartan36@gmail.com>
2023-02-22 15:17:41 +0300

    added send integration use case

---
+++ domain/src/main/java/com/sogya/domain/usecases/network/SendAppIntegrationUseCase.kt
@@ -0,0 +1,11 @@
+package com.sogya.domain.usecases.network
+
+import com.sogya.domain.models.DeviceDataDomain
+import com.sogya.domain.repository.NetworkRepository
+
+class SendAppIntegrationUseCase(
+    private val networkRepository: NetworkRepository
+) {
+    fun invoke(baseUri:String,token: String, deviceData: DeviceDataDomain) =
+        networkRepository.sendAppIntegration(baseUri,"Bearer $token", deviceData)
+}
