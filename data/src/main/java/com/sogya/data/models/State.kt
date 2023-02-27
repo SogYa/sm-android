ba637b51b6ed9fe69b9de7eb0382f57112081fdf
Timofey <Ghost.Spartan36@gmail.com>
2023-02-27 16:27:56 +0300

    chore

--- data/src/main/java/com/sogya/data/models/State.kt
+++ data/src/main/java/com/sogya/data/models/State.kt
@@ -2,7 +2,6 @@ package com.sogya.data.models
 
 import androidx.room.Embedded
 import androidx.room.Entity
-import androidx.room.Ignore
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
