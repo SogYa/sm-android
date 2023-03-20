39e79b17744dcd30352ace3c20c895b83279798d
Timofey <Ghost.Spartan36@gmail.com>
2023-03-20 12:57:29 +0300

    chore

--- domain/src/main/java/com/sogya/domain/usecases/websocketus/InitUseCase.kt
+++ domain/src/main/java/com/sogya/domain/usecases/websocketus/InitUseCase.kt
@@ -6,6 +6,6 @@ import com.sogya.domain.repository.WebSocketRepository
 class InitUseCase(
     private val webSocketRepository: WebSocketRepository
 ) {
-    fun invoke(baseUrl: String, _messageListenner: MessageListener) =
-        webSocketRepository.init(baseUrl, _messageListenner)
+    fun invoke(baseUrl: String, _messageListener: MessageListener) =
+        webSocketRepository.init(baseUrl, _messageListener)
 }
