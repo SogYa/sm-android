33183f09bb4472f6ecb959bd5f5cba469dc670bd
Timofey <Ghost.Spartan36@gmail.com>
2023-03-26 17:17:27 +0300

    added network connection observer

--- app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/MainActivity.kt
+++ app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/MainActivity.kt
@@ -1,7 +1,6 @@
 package ru.sogya.projects.smartrevolutionapp.screens
 
 import android.os.Bundle
-import android.util.Log
 import android.view.MenuItem
 import android.view.View.*
 import android.widget.TextView
