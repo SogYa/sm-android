1c9aa5c68668daa9083fef5ff33e0e63c1da620a
SogYa <Ghost.Spartan36@gmail.com>
2023-05-16 16:10:38 +0300

    added change menu type fun

--- app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/MainActivity.kt
+++ app/src/main/java/ru/sogya/projects/smartrevolutionapp/screens/MainActivity.kt
@@ -4,12 +4,10 @@ import android.os.Build
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View.*
-import android.widget.TextView
 import android.window.OnBackInvokedDispatcher
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.BuildCompat
-import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
+import androidx.core.view.size
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -25,9 +23,11 @@ class MainActivity : AppCompatActivity(), LogOutDialogFragment.DialogFragmentLis
     private lateinit var binding: ActivityMainBinding
     private lateinit var navHostFragment: NavHostFragment
     private lateinit var navController: NavController
-    private lateinit var serverUri: TextView
-    private lateinit var serverName: TextView
     private lateinit var vm: MainVM
+    companion object{
+        private const val SERVERS_MENU = 2
+        private const val MAIN_MENU = 4
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_SmartRevolutionApp)
@@ -40,34 +40,46 @@ class MainActivity : AppCompatActivity(), LogOutDialogFragment.DialogFragmentLis
         setupNavigation()
         //Android 13 predictive back gesture
         setupTiramisuBackPressed()
-        binding.hambutgerButton.setOnClickListener {
-            if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
-                binding.drawerLayout.closeDrawer(GravityCompat.START)
-            } else {
-                binding.drawerLayout.openDrawer(GravityCompat.START)
-            }
-        }
+
         vm = ViewModelProvider(this)[MainVM::class.java]
         navController.addOnDestinationChangedListener { _, destination, _ ->
             when (destination.id) {
                 R.id.authFragment, R.id.lockFragment, R.id.firebaseAuthFragment, R.id.firebaseRegistrationFragment -> {
                     supportActionBar?.hide()
-                    binding.drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
                     binding.bottomNav.visibility = GONE
                 }
 
                 R.id.firebaseAccountFragment, R.id.serversFragment -> {
+                    changeMenu(SERVERS_MENU)
                     supportActionBar?.hide()
-                    binding.drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
                     binding.bottomNav.visibility = VISIBLE
                 }
 
                 else -> {
+                    changeMenu(MAIN_MENU)
                     supportActionBar?.show()
-                    binding.bottomNav.visibility = GONE
+                    binding.bottomNav.visibility = VISIBLE
                 }
             }
         }
+    }
+     private fun changeMenu(menu:Int){
+         binding.apply {
+            when(menu){
+                MAIN_MENU ->{
+                    if(bottomNav.menu.size != MAIN_MENU){
+                        bottomNav.menu.clear()
+                        bottomNav.inflateMenu(R.menu.nav_drawer_menu)
+                    }
+                }
+                SERVERS_MENU ->{
+                    if(bottomNav.menu.size != SERVERS_MENU){
+                        bottomNav.menu.clear()
+                        bottomNav.inflateMenu(R.menu.bottom_nav_menu)
+                    }
+                }
+            }
+         }
     }
 
     private fun setupTiramisuBackPressed() {
@@ -87,11 +99,6 @@ class MainActivity : AppCompatActivity(), LogOutDialogFragment.DialogFragmentLis
             supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
         navController = navHostFragment.navController
         binding.bottomNav.setupWithNavController(navController)
-        binding.navView.setupWithNavController(navController)
-        //Передача URL в текст заголовка меню
-        serverUri = binding.navView.getHeaderView(0).findViewById(R.id.baseUrl)
-        serverName = binding.navView.getHeaderView(0).findViewById(R.id.serverName)
-        serverUri.isSelected = true
     }
 
     fun getServerState() {
@@ -100,12 +107,6 @@ class MainActivity : AppCompatActivity(), LogOutDialogFragment.DialogFragmentLis
 
     override fun onResume() {
         super.onResume()
-        vm.getServerNameLiveData().observe(this) {
-            serverName.text = it
-        }
-        vm.getServerUriLiveData().observe(this) {
-            serverUri.text = it
-        }
         vm.getNetworkStateLiveData().observe(this) {
             val dialogFragment = NetworkConnectionDialog()
             if (!it) {
