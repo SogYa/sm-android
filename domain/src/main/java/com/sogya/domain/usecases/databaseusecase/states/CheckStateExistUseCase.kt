5b6d2180628241ac9314100883b88f8a29b48352
SogYa <Ghost.Spartan36@gmail.com>
2023-06-18 22:23:46 +0300

    chore

--- domain/src/main/java/com/sogya/domain/usecases/databaseusecase/states/CheckStateExistUseCase.kt
+++ domain/src/main/java/com/sogya/domain/usecases/databaseusecase/states/CheckStateExistUseCase.kt
@@ -2,7 +2,7 @@ package com.sogya.domain.usecases.databaseusecase.states
 
 import com.sogya.domain.repository.LocalDataBaseRepository
 
-class CheckStateExistUSeCase(
+class CheckStateExistUseCase(
     private val repository: LocalDataBaseRepository
 ) {
     fun invoke(stateId:String) = repository.isStateInDB(stateId)
