aad42aa226c04cfcf97bc563ef802b4cfe3846f0
SogYa <Ghost.Spartan36@gmail.com>
2023-06-28 13:10:27 +0300

    миграция с RxJava на Flow

--- data/src/main/java/com/sogya/data/network/api/HomeAssistantApi.kt
+++ data/src/main/java/com/sogya/data/network/api/HomeAssistantApi.kt
@@ -3,42 +3,41 @@ package com.sogya.data.network.api
 
 import com.sogya.data.models.IntegrationResponseData
 import com.sogya.data.models.MessageData
-import com.sogya.data.models.State
+import com.sogya.data.models.StateData
 import com.sogya.data.models.requests.IntegrationRequestData
 import com.sogya.domain.models.TokenInfo
-import io.reactivex.Single
 import retrofit2.http.*
 
 interface HomeAssistantApi {
     @GET("/api/")
-    fun getApiMessage(@Header("Authorization") token: String): Single<MessageData>
+    suspend fun getApiMessage(@Header("Authorization") token: String): MessageData
 
     @GET("/api/states")
-    fun getApiStates(@Header("Authorization") token: String): Single<List<State>>
+    suspend fun getApiStates(@Header("Authorization") token: String): List<StateData>
 
     @GET("/api/history/period/{timestamp}?filter_entity_id={entityId}")
-    fun getStateHistory(
+    suspend fun getStateHistory(
         @Header("Authorization") token: String,
         @Path("timestamp") timestamp: String,
         @Path("entityId") entityId: String
-    ):Single<List<State>>
+    ): List<StateData>
 
 
     @FormUrlEncoded
     @POST("/auth/token")
-    fun getApiToken(
+    suspend fun getApiToken(
         @Field("grant_type") grandType: String,
         @Field("code") code: String,
         @Field("client_id") clientId: String
-    ): Single<TokenInfo>
+    ): TokenInfo
 
     @POST("/api/mobile_app/registrations")
-    fun registrationMobileAppIntegration(
+    suspend fun registrationMobileAppIntegration(
         @Header("Authorization") token: String, @Body device: IntegrationRequestData
-    ): Single<IntegrationResponseData>
+    ): IntegrationResponseData
 
     @GET("/api/states/{entityId}")
-    fun getStateById(
+    suspend fun getStateById(
         @Header("Authorization") token: String, @Path("entityId") entityId: String
-    ): Single<State>
+    ): StateData
 }
--- data/src/main/java/com/sogya/data/repository/NetworkRepositoryImpl.kt
+++ data/src/main/java/com/sogya/data/repository/NetworkRepositoryImpl.kt
@@ -1,77 +1,74 @@
 package com.sogya.data.repository
 
-import com.sogya.data.mappers.message.MessageMapper
-import com.sogya.data.mappers.state.ListOfStatesMapper
-import com.sogya.data.mappers.state.StatesMapper
 import com.sogya.data.models.requests.IntegrationRequestData
 import com.sogya.data.network.api.NetworkService
 import com.sogya.domain.models.DeviceDataDomain
 import com.sogya.domain.models.IntegrationResponseDomain
-import com.sogya.domain.models.Message
+import com.sogya.domain.models.MessageDomain
 import com.sogya.domain.models.StateDomain
 import com.sogya.domain.models.TokenInfo
 import com.sogya.domain.repository.NetworkRepository
-import io.reactivex.Single
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class NetworkRepositoryImpl : NetworkRepository {
 
-    override fun getMessage(baseUri: String, token: String): Single<Message> {
-        return NetworkService.getRetrofitService(baseUri).getApiMessage(token).map {
-            return@map MessageMapper(it).toMessageDomian()
-        }
+    override suspend fun getMessage(baseUri: String, token: String): Flow<MessageDomain> = flow {
+        emit(NetworkService.getRetrofitService(baseUri).getApiMessage(token))
     }
 
-    override fun getToken(baseUri: String, authCode: String): Single<TokenInfo> {
-        return NetworkService.getRetrofitService(baseUri)
-            .getApiToken("authorization_code", authCode, baseUri)
+    override suspend fun getToken(baseUri: String, authCode: String): Flow<TokenInfo> = flow {
+        emit(
+            NetworkService.getRetrofitService(baseUri)
+                .getApiToken("authorization_code", authCode, baseUri)
+        )
     }
 
-    override fun getStates(baseUri: String, token: String): Single<List<StateDomain>> {
-        return NetworkService.getRetrofitService(baseUri)
-            .getApiStates(token).map {
-                return@map ListOfStatesMapper(it).toDomainList()
-            }
+    override suspend fun getStates(baseUri: String, token: String): Flow<List<StateDomain>> = flow {
+        emit(
+            NetworkService.getRetrofitService(baseUri)
+                .getApiStates(token)
+        )
     }
 
-    override fun getStateById(
+    override suspend fun getStateById(
         baseUri: String,
         token: String,
         entityId: String
-    ): Single<StateDomain> {
-        return NetworkService.getRetrofitService(baseUri).getStateById(token, entityId).map {
-            return@map StatesMapper(it).toStateDomain()
-        }
+    ): Flow<StateDomain> = flow {
+        emit(NetworkService.getRetrofitService(baseUri).getStateById(token, entityId))
     }
 
-    override fun sendAppIntegration(
+    override suspend fun sendAppIntegration(
         baseUri: String,
         token: String,
         deviceData: DeviceDataDomain
-    ): Single<IntegrationResponseDomain> {
-        return NetworkService.getRetrofitService(baseUri).registrationMobileAppIntegration(
-            token,
-            IntegrationRequestData(
-                deviceId = deviceData.deviceId,
-                deviceName = deviceData.deviceName,
-                manufacturer = deviceData.manufacturer,
-                model = deviceData.model,
-                operationSystemName = deviceData.operationSystemName,
-                operationSystemVersion = deviceData.operationSystemVersion
+    ): Flow<IntegrationResponseDomain> = flow {
+        emit(
+            NetworkService.getRetrofitService(baseUri).registrationMobileAppIntegration(
+                token,
+                IntegrationRequestData(
+                    deviceId = deviceData.deviceId,
+                    deviceName = deviceData.deviceName,
+                    manufacturer = deviceData.manufacturer,
+                    model = deviceData.model,
+                    operationSystemName = deviceData.operationSystemName,
+                    operationSystemVersion = deviceData.operationSystemVersion
+                )
             )
-        ).map {
-            return@map it.toDomain()
-        }
+        )
     }
 
-    override fun getStateHistory(
+    override suspend fun getStateHistory(
         baseUri: String,
         token: String,
         timestamp: String,
         entityId: String
-    ): Single<List<StateDomain>> {
-        return NetworkService.getRetrofitService(baseUri)
-            .getStateHistory(token, timestamp, entityId).map {
-            return@map ListOfStatesMapper(it).toDomainList()
-        }
+    ): Flow<List<StateDomain>> = flow {
+        emit(
+            NetworkService.getRetrofitService(baseUri)
+                .getStateHistory(token, timestamp, entityId)
+        )
+
     }
 }
